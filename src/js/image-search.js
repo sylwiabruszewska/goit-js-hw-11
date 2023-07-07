47bf7067defb12104ba137ba0a0b3e5110f53e8c
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-07-07 17:26:29 +0200

    feat: add function to render images

--- src/js/image-search.js
+++ src/js/image-search.js
@@ -10,20 +10,56 @@ const galleryElement = document.querySelector('.gallery');
 async function getImages() {
   const searchParams = new URLSearchParams({
     key: API_KEY,
-    q: inputElement.value,
+    q: encodeURIComponent(inputElement.value),
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
   });
   const url = `https://pixabay.com/api/?${searchParams}`;
   const response = await axios.get(url);
-  const images = response.data;
-  console.log(images);
+  console.log(response.data.hits);
+  return response.data.hits;
 }
 
 formElement.addEventListener('submit', onSubmit);
 
-function onSubmit(event) {
+async function onSubmit(event) {
   event.preventDefault();
-  getImages();
+  let images = await getImages();
+  renderImages(images);
+  inputElement.value = '';
+}
+
+function renderImages(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+        <div class="photo-card">
+            <img class="gallery__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+                <p class="info-item">
+                    <b>Likes:</b> ${likes}
+                </p>
+                <p class="info-item">
+                    <b>Views:</b> ${views}
+                </p>
+                <p class="info-item">
+                    <b>Comments:</b> ${comments}
+                </p>
+                <p class="info-item">
+                    <b>Downloads:</b> ${downloads}
+                </p>
+            </div>
+        </div>`
+    )
+    .join('');
+  galleryElement.innerHTML = markup;
 }
